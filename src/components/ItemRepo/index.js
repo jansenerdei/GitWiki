41f86f6e8092b3a77214b3dad0537fd0311624e1
Jansen Erdei <jansenerdei@gmail.com>
2023-08-23 14:31:46 -0300

    Primeira versão

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ItemContainer } from './styles';
+
+function ItemRepo({repo}) {
+  return (
+    <ItemContainer>
+      <h3>{repo.name}</h3>
+      <p>{repo.full_name}</p>
+      <a href={repo.html_url} rel="noreferrer" target="_blank" >Ver Repositório</a><br />
+      <a href='#' className='remover'>Remover</a>
+      <hr />
+   </ItemContainer>
+  )
+}
+
+export default ItemRepo;
---
+++ src/pages/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import gitLogo from '../assets/github2.png';
+import Input from '../components/input';
+import Button from '../components/Button';
+import ItemRepo from '../components/ItemRepo';
+import {Container} from './styles';
+import {api} from "../services/api";
+
+function App() {
+
+  const [currentRepo, setCurrentRepo] = useState('');
+  const [repos, setRepos] = useState([]);
+
+  const handleSearchRepo = async () => {
+
+    const {data} = await api.get(`repos/${currentRepo}`)
+
+    if(data.id){
+
+      const isExist = repos.find(repo => repo.id === data.id)
+
+      if(!isExist){
+        setRepos(prev =>[...prev, data]);
+        setCurrentRepo('')
+      }
+      return
+    }
+    alert('Repositório não encontrado!')
+
+  }
+
+  return (
+    <Container>
+      <img src={gitLogo} width={80} height={72} alt='Git Logo'/>
+      <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)} />
+      <Button onClick={handleSearchRepo} />
+      {repos.map(repo => <ItemRepo repo={repo} />)}
+    </Container>
+  );
+}
+
+export default App;
